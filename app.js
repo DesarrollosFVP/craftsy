d51930eabe6ab2b7abc862acae850b6e753f16d3
DesarrollosFVP <federico.villalba.petrelli@gmail.com>
2023-08-06 19:54:44 -0300

    refactor: EJS

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const express = require("express");
-const path = require("path");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const app = express();
-const PORT = 3030;
 
-app.use(express.static('public'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-/* rutas */
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "home.html"))
-);
-app.get("/header", (req, res) =>
-  res.sendFile(path.join(__dirname, "views","partials", "header.html"))
-);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/register", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "register.html"))
-);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.get("/login", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "login.html"))
-);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.get("/profile", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "profile.html"))
-);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(PORT, () =>
-  console.log("Server running in http://localhost:" + PORT)
-);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
